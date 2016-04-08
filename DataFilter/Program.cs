dac23f51d0d3c80e992a37300fd87b968a3f2a62
Farzan.majdani <farzan.majdani@dai.co.uk>
2016-04-08 09:47:00 +0100

    Process change

--- DataFilter/Program.cs
+++ DataFilter/Program.cs
@@ -4,27 +4,67 @@ using System.Data;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Linq;
 using GenericParsing;
+using System.Text;
+using System.IO;
 
 namespace DataFilter
 {
     class Program
     {
         private static string path = @"C:\temp\pump-data.csv";
+        private static string textPath = @"C:\temp\result.txt";
+        private static string csvPath = @"C:\temp\result.csv";
         private static Dictionary<string, double> maxDic = new Dictionary<string, double>();
         private static Dictionary<string, double> minDic = new Dictionary<string, double>();
-        private static double percentage = 0.9;
+        private static double percentage = 4;
         static void Main(string[] args)
         {
             DataTable dt = ConvertToDataTable(path);
             setMinAndMax(dt);
-            Dictionary<string, double> resultDic = GetResult(dt);
+            var possibilities = GetPosibilities(11);
+            Dictionary<string, double> resultDic = GetResult(dt, possibilities);
             WriteOutTheFile(resultDic);
+
+            DataTable resultTable = GenerateResultDataTable(resultDic, dt.Columns);
+            DatatableToCsv(resultTable);
+        }
+
+        private static DataTable GenerateResultDataTable(Dictionary<string, double> resultDic, DataColumnCollection datacolumnCollection)
+        {
+            DataTable dataTable = new DataTable();
+
+            datacolumnCollection[datacolumnCollection.Count - 1].ColumnName = "Posibilities";
+            datacolumnCollection.Add("Performance");
+
+            foreach(DataColumn column in datacolumnCollection)
+            {
+                dataTable.Columns.Add(column.ColumnName);
+            }
+
+            foreach (string posibility in resultDic.Keys)
+            {
+                DataRow row = dataTable.NewRow();
+
+                char[] tokens = posibility.ToCharArray();
+                for (int i = 0; i < tokens.Length; i++)
+                {
+                    var colName = datacolumnCollection[i].ColumnName;
+                    row[colName] = tokens[i] == 'L' ? minDic[colName] : maxDic[colName];
+                }
+
+                row[datacolumnCollection.Count - 2] = posibility;
+                row[datacolumnCollection.Count - 1] = resultDic[posibility];
+
+                dataTable.Rows.Add(row);
+            }
+
+            return dataTable;
         }
 
         private static void WriteOutTheFile(Dictionary<string, double> dic)
         {
             using (System.IO.StreamWriter file =
-            new System.IO.StreamWriter(@"C:\temp\Result.txt", false))
+            new System.IO.StreamWriter(textPath, false))
             {
                 file.WriteLine("-----Max Values -----");
                 foreach (string key in maxDic.Keys)
@@ -70,19 +110,17 @@ namespace DataFilter
             }
         }
 
-        private static Dictionary<string, double> GetResult(DataTable dt)
+        private static Dictionary<string, double> GetResult(DataTable dt, List<string> possibilities)
         {
             Dictionary<string, double> resultDic = new Dictionary<string, double>();
-            Dictionary<string, double> temp = new Dictionary<string, double>();
             Dictionary<string, int> ColumnLowHigh = new Dictionary<string, int>();
-
-            string[] possibilities = { "LLLL", "HLHH", "LLHL", "HHHH", "HLLL", "LHLH", "LLHH", "LHHH", "HHLH", "HLLH", "HHHL", "HHLL", "LLLH", "LHLL", "HLHL", "LHHL" };
 
             for (int r = 0; r < dt.Rows.Count; r++)
             {
-                double performance = Convert.ToDouble(dt.Rows[r][dt.Columns.Count - 5]);
+                double performance = Convert.ToDouble(dt.Rows[r][dt.Columns.Count - 1]);
+                ColumnLowHigh.Clear();
 
-                for (int c = 0; c <= dt.Columns.Count - 2; c++)
+                for (int c = 0; c < dt.Columns.Count - 1; c++)
                 {
                     string colName = dt.Columns[c].ColumnName;
                     double value = Convert.ToDouble(dt.Rows[r][c]);
@@ -104,42 +142,85 @@ namespace DataFilter
 
                     if (ShouldConsider == 1)
                     {
-                        temp.Add(possibility + "-" + r, performance);
+                        if (resultDic.ContainsKey(possibility))
+                        {
+                            var occaranceOfLow = ((double)possibility.Count(x => x == 'L')) / possibility.Length;
+
+                            if (occaranceOfLow > 0.5)
+                            {
+                                if (performance > resultDic[possibility])
+                                {
+                                    resultDic[possibility] = performance;
+                                }
+
+                            }
+                            else if (occaranceOfLow < 0.5)
+                            {
+                                if (performance < resultDic[possibility])
+                                {
+                                    resultDic[possibility] = performance;
+                                }
+                            }
+                            else
+                            {
+                                if (performance != resultDic[possibility])
+                                {
+                                    resultDic[possibility] = (performance + resultDic[possibility]) / 2;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            resultDic.Add(possibility, percentage);
+                        }
                     }
                 }
-
-                ColumnLowHigh.Clear();
             }
 
-            foreach (string possibility in possibilities)
+            foreach (var posibility in possibilities)
             {
-                char[] token = possibility.ToCharArray();
-                var Ls = token.Count(x => x == 'L');
-                double max = 0;
-                var list = temp.Where(p => p.Key.Contains(possibility)).Select(x => x.Value).DefaultIfEmpty(0);
-                switch (Ls)
+                if (!resultDic.ContainsKey(posibility))
                 {
-                    case 0:
-                        max = list.Max();
-                        break;
-                    case 1:
-                        max = list.Max();
-                        break;
-                    case 2:
-                        max = list.Average();
-                        break;
-                    case 3:
-                        max = list.Min();
-                        break;
-                    case 4:
-                        max = list.Min();
-                        break;
+                    resultDic[posibility] = 0;
                 }
-
-                resultDic.Add(possibility, max);
             }
 
             return resultDic;
+        }
+
+        private static List<string> GetPosibilities(int size)
+        {
+            var list = new List<string>();
+            var alphabet = "LH";
+            var q = alphabet.Select(x => x.ToString());
+            for (int i = 0; i < size - 1; i++)
+            {
+                q = q.SelectMany(x => alphabet, (x, y) => x + y);
+            }
+
+            foreach (var item in q)
+            {
+                list.Add(item);
+            }
+
+            return list;
+        }
+
+        private static void DatatableToCsv(DataTable dt)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            IEnumerable<string> columnNames = dt.Columns.Cast<DataColumn>().
+                                              Select(column => column.ColumnName);
+            sb.AppendLine(string.Join(",", columnNames));
+
+            foreach (DataRow row in dt.Rows)
+            {
+                IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
+                sb.AppendLine(string.Join(",", fields));
+            }
+
+            File.WriteAllText(csvPath, sb.ToString());
         }
     }
 }
